2ff158ef530c0b0c983d56e1dce6a70af0d83e6b
Miaopashi <05181808@cumt.edu.cn>
2021-06-22 21:37:24 +0800

    使用了子窗口的方式显示动图图标

--- src/main/java/DogCatCounter.java
+++ src/main/java/DogCatCounter.java
@@ -3,12 +3,14 @@ import java.awt.*;
 import java.util.Objects;
 
 public class DogCatCounter extends JFrame {
-    private final JFrame frame = new JFrame();  // 主窗口画板
+    private final JFrame frame = new JFrame();  // 主窗口
+    private final JFrame childFrame = new JFrame();  // 子窗口
     private final JPanel panel1 = new JPanel();  // 窗口组件
     private final JPanel panel2 = new JPanel();
     private final JPanel panel3 = new JPanel();
     private final JPanel panel4 = new JPanel();
-    private final JPanel panelAll = new JPanel(new GridLayout(4, 0));  // 控制窗口子组件的布局
+    private final JPanel panelMain = new JPanel(new GridLayout(2, 0));  // 控制窗口子组件的布局
+    private final JPanel panelChild = new JPanel(new GridLayout(2, 0));
     private final JTextField text1 = new JTextField(10);  // 计数文本
     private final JTextField text2 = new JTextField(10);
     private final JButton start1 = new JButton("Start");  // 开始按钮
@@ -22,8 +24,11 @@ public class DogCatCounter extends JFrame {
 
     private void AppearanceSetting() {
         // 设置主窗口大小
-        frame.setSize(1200, 310);
+        frame.setSize(400, 130);
+        frame.setLocation(400, 200);
         frame.setLayout(new FlowLayout());
+        childFrame.setSize(1920, 200);
+        childFrame.setLayout(new FlowLayout());
     }
 
 
@@ -41,26 +46,25 @@ public class DogCatCounter extends JFrame {
         panel2.add(stop2);
         panel2.setLayout(new FlowLayout());
         // 根据布局的层次添加子组件到父组件中
-        panelAll.add(panel3);
-        panelAll.add(panel1);
-        panelAll.add(panel4);
-        panelAll.add(panel2);
+        panelMain.add(panel1);
+        panelMain.add(panel2);
         // 将组件添加到主窗口
-        frame.add(panelAll);
+        frame.add(panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        // 添加组件到子窗口
+        panelChild.add(panel3);
+        panelChild.add(panel4);
+        childFrame.add(panelChild);
     }
 
 
     private void Jb1Listener1() {
         // 监听计数器1的start按钮事件
         start1.addActionListener(e -> {
-
             Flag1 = true;  // 开始计数
-
             new Thread(() -> {
                 while (true) {
-
                     if (Flag1) {
                         try {
                             Thread.sleep(200);  // 线程暂停200ms
@@ -74,8 +78,9 @@ public class DogCatCounter extends JFrame {
                                 // 计数变量每增加10次，添加一个动物图标并且刷新主窗口
                                 panel3.add(new JLabel(new ImageIcon(Objects.requireNonNull(getClass()
                                         .getResource("/img/cat.png")))));
-                                frame.validate();
-                                frame.repaint();
+                                childFrame.setVisible(true);
+                                childFrame.validate();
+                                childFrame.repaint();
                             }
                             text1.setText(String.valueOf(count1));
                         });
@@ -96,12 +101,9 @@ public class DogCatCounter extends JFrame {
     private void Jb1Listener2() {
         // 监听计数器2的start按钮事件
         start2.addActionListener(e -> {
-
             Flag2 = true;
-
             new Thread(() -> {
                 while (true) {
-
                     if (Flag2) {
                         try {
                             Thread.sleep(100);
@@ -114,8 +116,9 @@ public class DogCatCounter extends JFrame {
                             if (count2 % 10 == 0) {
                                 panel4.add(new JLabel(new ImageIcon(Objects.requireNonNull(getClass()
                                         .getResource("/img/dog.png")))));
-                                frame.validate();
-                                frame.repaint();
+                                childFrame.setVisible(true);
+                                childFrame.validate();
+                                childFrame.repaint();
                             }
                             text2.setText(String.valueOf(count2));
                         });
